98f91a08e293b4be25cd9d657109fd827040f635
Daniel Amorim <amorimdaniel0906@gmail.com>
2022-09-08 14:38:09 -0300

    Add files via upload
    site formatado para abrir no servidor web hospedado

---
+++ HairExe-3/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Página Inicial</title>
+</head>
+<body>
+	<h1>Página Inicial</h1>
+	<p><a href="https://hairexe.000webhostapp.com/cadastro_usuario.php">Cadastro</a></p>
+	<p><a href="https://hairexe.000webhostapp.com/login_usuario.php">Login</a></p>
+</body>
+</html>
